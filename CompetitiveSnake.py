0eabcb7136079d6d8ce96af560fc08a5410a59c1
George Shao <GeorgeShao123@gmail.com>
2019-09-29 21:45:43 -0400

    Created button class to simplify button related programming

--- CompetitiveSnake.py
+++ CompetitiveSnake.py
@@ -4,6 +4,42 @@ import arcade
 WIDTH = 800
 HEIGHT = 600
 
+# TODO: create general button class
+
+class Button:
+    def __init__(x, y, w, h, color, filled, buttontype):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.color = color
+        self.filled = filled
+        self.type = buttontype
+    
+    def draw_button(self):
+        if self.filled:
+            if self.buttontype == "standard":
+                arcade.draw_rectangle_filled(self.x, self.y, self.w, self.h, self.color)
+            elif self.buttontype == "ltrb":
+                arcade.draw_lrtb_rectangle_filled(self.x, self.y, self.w, self.h, self.color)
+            elif self.buttontype == "xywh":
+                arcade.draw_xywh_rectangle_filled(self.x, self.y, self.w, self.h, self.color)
+        if not self.filled:
+            if self.buttontype == "standard":
+                arcade.draw_rectangle_outline(self.x, self.y, self.w, self.h, self.color)
+            elif self.buttontype == "ltrb":
+                arcade.draw_lrtb_rectangle_outline(self.x, self.y, self.w, self.h, self.color)
+            elif self.buttontype == "xywh":
+                arcade.draw_xywh_rectangle_outline(self.x, self.y, self.w, self.h, self.color)
+
+
+# Start screen variables
+on_start_screen = True
+start_key_pressed = False
+start_key_rgb = arcade.color.GREEN
+start_rgb_rgb_target = arcade.color.GO_GREEN
+
+on_play_screen = False
 
 def on_update(delta_time):
     pass
@@ -11,15 +47,26 @@ def on_update(delta_time):
 
 def on_draw():
     arcade.start_render()
-    draw_grid()
+    if on_start_screen:
+        draw_start_screen()
+    elif on_play_screen:
+        draw_grid()
+
+
+def draw_start_screen():
+    global start_key_pressed
+    if not start_key_pressed:
+        arcade.draw_rectangle_filled(WIDTH/2, HEIGHT/2, 300, 100, arcade.color.GREEN)
+    elif start_key_pressed:
+        arcade.draw_rectangle_filled(WIDTH/2, HEIGHT/2, 300, 100, arcade.color.GO_GREEN)
 
 
 def draw_grid():
-    arcade.draw_xywh_rectangle_outline(1, 1, WIDTH-1, HEIGHT-2, color=arcade.color.RED)
+    arcade.draw_xywh_rectangle_outline(1, 1, WIDTH-1, HEIGHT-2, arcade.color.RED)
     for i in range(int(WIDTH/20)):
-        arcade.draw_line(i*20, 0, i*20, HEIGHT, color=arcade.color.BLACK)
+        arcade.draw_line(i*20, 0, i*20, HEIGHT, arcade.color.BLACK)
     for i in range(int(HEIGHT/20)):
-        arcade.draw_line(0, i*20, WIDTH, i*20, color=arcade.color.BLACK)
+        arcade.draw_line(0, i*20, WIDTH, i*20, arcade.color.BLACK)
 
 
 def on_key_press(key, modifiers):
@@ -31,7 +78,10 @@ def on_key_release(key, modifiers):
 
 
 def on_mouse_press(x, y, button, modifiers):
-    pass
+    global start_key_pressed
+    if WIDTH/2 - 150 <= x <= WIDTH/2 + 150:
+        if HEIGHT/2 - 50 <= y <= HEIGHT/2 + 50:
+            start_key_pressed = True
 
 
 def setup():
