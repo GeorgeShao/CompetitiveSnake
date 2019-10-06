2d55d7043d581010eb34735d237181f4fc38ece2
unknown <frankfrankfrankyeyeye@gmail.com>
2019-10-06 19:17:01 -0400

    Did everything

--- temp.py
+++ temp.py
@@ -1,11 +1,16 @@
+import logging
+import os
 import random
 import arcade
-import os
-import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
-SPRITE_SCALING = 0.5
+UPDATE_HERTZ = 4
+
+SNAKE_1_CONTROLS = {arcade.key.UP: 0, arcade.key.RIGHT: 1, arcade.key.DOWN: 2, arcade.key.LEFT: 3}
+SNAKE_1_COLOR = arcade.color.GREEN
+SNAKE_2_CONTROLS = {arcade.key.W: 0, arcade.key.D: 1, arcade.key.S: 2, arcade.key.A: 3}
+SNAKE_2_COLOR = arcade.color.YELLOW
 
 SCREEN_WIDTH = WIDTH = 800
 SCREEN_HEIGHT = HEIGHT = 600
@@ -18,7 +23,7 @@ class Button:
         self.x = x
         self.y = y
         self.w = w
-        self.h = h
+        self.h = hz
         self.color = color
         self.filled = filled
         self.buttontype = buttontype
@@ -80,20 +85,24 @@ class MyGame(arcade.Window):
             "on_start_screen": True,
             "on_play_screen":  False,
             "start_key_pressed": False,
-            "color_code": 255
+            "color_code": 255,
+            "snake_direction_1": 0,
+            "snake_direction_2": 0,
+            "win_state": 0
         }
         self.background = None
-
+        self.snake1 = [(1,1), (1,2), (1,3), (1,4), (1,5)]
+        self.snake2 = [(38,1), (38,2), (38,3), (38,4), (38, 5)]
         arcade.set_background_color(arcade.color.WHITE)
         self.start_button = Button(WIDTH/2, HEIGHT/2, 300, 100, arcade.color.GREEN, True, "standard")
         self.start_button.text_arguments("PLAY", WIDTH/2 - 60, HEIGHT/2 - 25, arcade.color.WHITE, font_size=50)
 
-    def _draw_tile(self, tile):
-        arcade.draw_xywh_rectangle_filled(20*tile[0] + 1, 20*tile[1] + 1, 19, 19, arcade.color.GREEN)
+    def _draw_tile(self, tile, color):
+        arcade.draw_xywh_rectangle_filled(20*tile[0] + 1, 20*tile[1] + 1, 19, 19, color)
     
-    def draw_snake(self, snake):
+    def draw_snake(self, snake, color):
         for i in snake:
-            self._draw_tile(i)
+            self._draw_tile(i, color)
 
     def on_draw(self):
         arcade.start_render()
@@ -109,24 +118,32 @@ class MyGame(arcade.Window):
         elif self.flags["on_play_screen"]:
             arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
                                          SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
-            self.draw_snake([(1,1), (1,2), (2,2)])
+            self.draw_snake(self.snake1, SNAKE_1_COLOR)
+            self.draw_snake(self.snake2, SNAKE_2_COLOR)
+        elif self.flags["win_state"] == 1:
+            print("Win state 1, exiting...")
+            arcade.close_window()
+        elif self.flags["win_state"] == 2:
+            print("Win state 2, exiting...")
+            arcade.close_window()
 
 
     def draw_start_screen(self):
         self.start_button.draw()
 
 
-    def draw_start_screen_animation(self):
-        arcade.draw_xywh_rectangle_filled(0, 0, WIDTH, HEIGHT, start_key_rgb)
-
-        if self.flags["on_start_screen"]:
-            arcade.set_background_color(arcade.color.WHITE)
-        elif self.flags["on_play_screen"]:
-            arcade.set_background_color(arcade.color.BLACK)
-
-
     def on_key_press(self, key, modifiers):
-        pass
+        if self.flags["on_play_screen"]:
+            try:
+                self.flags["snake_direction_1"] = SNAKE_1_CONTROLS[key]
+            except KeyError:
+                print("Key error 1")
+                pass
+            try:
+                self.flags["snake_direction_2"] = SNAKE_2_CONTROLS[key]
+            except KeyError:
+                print("Key error 2")
+                pass
 
 
     def on_key_release(self, key, modifiers):
@@ -141,7 +158,49 @@ class MyGame(arcade.Window):
 
 
     def update(self, delta_time):
-        pass
+        if self.flags["on_play_screen"]:
+            *snake_body, snake_head = self.snake1
+            print(snake_head)
+            if snake_head[1] < 0 or snake_head[1] > 28 or \
+                snake_head[0] < 0 or snake_head[0] > 39 or \
+                snake_head in snake_body or \
+                snake_head in self.snake2:
+                self.flags["on_play_screen"] = False
+                self.flags["win_state"] = 2
+                
+
+            self.set_update_rate(1/UPDATE_HERTZ)
+            if self.flags["snake_direction_1"] == 0:
+                self.snake1.append((snake_head[0], snake_head[1] + 1))
+            elif self.flags["snake_direction_1"] == 1:
+                self.snake1.append((snake_head[0] + 1, snake_head[1]))
+            elif self.flags["snake_direction_1"] == 2:
+                self.snake1.append((snake_head[0], snake_head[1] - 1))
+            elif self.flags["snake_direction_1"] == 3:
+                self.snake1.append((snake_head[0] - 1, snake_head[1]))
+            del self.snake1[0]
+
+
+            *snake_body, snake_head = self.snake2
+            print(snake_head)
+            if snake_head[1] < 0 or snake_head[1] > 28 or \
+                snake_head[0] < 0 or snake_head[0] > 39 or \
+                snake_head in snake_body or \
+                snake_head in self.snake1:
+                self.flags["on_play_screen"] = False
+                self.flags["win_state"] = 1
+                
+
+            self.set_update_rate(1/UPDATE_HERTZ)
+            if self.flags["snake_direction_2"] == 0:
+                self.snake2.append((snake_head[0], snake_head[1] + 1))
+            elif self.flags["snake_direction_2"] == 1:
+                self.snake2.append((snake_head[0] + 1, snake_head[1]))
+            elif self.flags["snake_direction_2"] == 2:
+                self.snake2.append((snake_head[0], snake_head[1] - 1))
+            elif self.flags["snake_direction_2"] == 3:
+                self.snake2.append((snake_head[0] - 1, snake_head[1]))
+            del self.snake2[0]
 
 
     def setup(self):
@@ -152,7 +211,10 @@ class MyGame(arcade.Window):
 def main():
     window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
     window.setup()
-    arcade.run()
+    try:
+        arcade.run()
+    except AttributeError:
+        print("Exiting with no errors.")  # If you see this text, it exited with a lot of errors and I don't know why.
 
 
 if __name__ == "__main__":
